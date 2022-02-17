54b1b80e23df16f5b8c42bf962c993de78c63fc5
EsquivelDaniel <careca_esquivel@hotmail.com>
2022-02-17 20:29:20 -0300

    Commit Inicial

---
+++ cursoReact/JavaScript/function.js
@@ -0,0 +1,22 @@
+// function Saludo(){
+//     console.log('Hola JavaScript')
+// }
+
+// Saludo()
+
+
+// const Saludos = () => {
+//     console.log('Hola JavaScript desde Arrow Functions')
+// }
+
+// Saludos()
+
+const function1 = (dato) => {
+    return dato
+}
+
+const function2 = (d) => {
+    console.log(d)
+}
+
+function2(function1('Careca'))
